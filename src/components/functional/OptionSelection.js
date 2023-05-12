1fdb1240db659ffb85bbdb0e44bd4ac087de2dab
gregarsua <arsuagreg@gmail.com>
2023-05-12 11:31:30 +0800

    add first page without the calendar

---
+++ src/components/functional/OptionSelection.js
@@ -0,0 +1,82 @@
+import { Fragment, useState } from "react";
+
+export default function OptionSelection() {
+  const [isChecked, setIsChecked] = useState(false);
+
+  function handleChange() {
+    setIsChecked(!isChecked);
+  }
+
+  const advancedOptions = isChecked ? (
+    <div className="mb-16 md:px-0 px-5">
+      <p className="md:text-lg text-sm">
+        このオプションは以下の詳細設定が可能です。
+      </p>
+      <div className="md:flex mt-4">
+        <div className="md:w-1/2 flex md:bg-[#EDEDED] p-5 justify-between md:mr-3">
+          <p className="text-lg">提供方法</p>
+          <select
+            className="bg-white border border-[#8E8E8E] rounded md:max-w-[328px] w-[175px]"
+            name=""
+            id=""
+          >
+            <option value="ネコロボ">ネコロボ</option>
+          </select>
+        </div>
+        <div className="md:w-1/2 flex md:bg-[#EDEDED] p-5 justify-between md:ml-3">
+          <p className="text-lg">提供時間</p>
+          <select
+            className="bg-white border border-[#8E8E8E] rounded md:max-w-[328px] w-[175px]"
+            name=""
+            id=""
+          >
+            <option value="15分後">15分後</option>
+          </select>
+        </div>
+      </div>
+      <p className="text-xs mt-5">
+        ※提供時間は、着席からの経過時間での設定となります。その他、ご要望などがある場合には「ご来店者情報」の「ご要望」欄に記載してください。
+      </p>
+    </div>
+  ) : null;
+
+  return (
+    <Fragment>
+      <div className="mt-4 mb-20 lg:px-0 px-5">
+        <p className="text-lg leading-[22px]">
+          オプション選択{" "}
+          <span className="text-xs text-white bg-[#ABABAB] font-semibold rounded p-[3px] pl-[6px] ml-2">
+            必須
+          </span>
+        </p>
+        <p className="text-xs mt-3">
+          ※不要な場合はチェック無し。※ケーキは1グループに対して1個となります。複数個ご注文される場合は、お手数ですがお電話にてご連絡をお願いいたします。
+        </p>
+        <div className="rounded shadow-[2px_2px_4px_3px_rgba(0,0,0,0.1)] mt-7 bg-white p-6 flex items-center justify-between">
+          <div className="flex items-center">
+            <input
+              type="checkbox"
+              className="checkbox rounded border border-[#8E8E8E] bg-white w-6 h-6"
+              onChange={handleChange}
+            />
+            <div className="flex items-center px-6">
+              <p className="md:pr-7 pr-4">IMAGE</p>
+              <div>
+                <p className="md:text-lg text-[13px] font-bold">
+                  【記念日のお祝いに】アニバーサリーケーキ　＋330円(税込)
+                </p>
+                <p className="md:text-sm text-[10px]">
+                  利用人数：2名～　予約締切：来店日の2日前21時まで
+                </p>
+              </div>
+            </div>
+          </div>
+          <p className="text-[#04512A] text-lg underline underline-offset-2 self-center">
+            詳細
+          </p>
+        </div>
+      </div>
+      {advancedOptions}
+    </Fragment>
+  );
+}
--- src/pages/reservation/index.js
+++ src/pages/reservation/index.js
@@ -1,45 +1,78 @@
-import { Fragment } from "react";
-import Steps from "../../components/layout/Steps";
-import ReservationForm from "../../components/ReservationForm";
+import { Fragment, useState } from "react";
+import Button from "../../components/ui/Button";
+import OptionSelection from "../../components/functional/OptionSelection";
 
 export default function ReservationPage() {
+  const [count, setCount] = useState(0);
+  const [isChecked, setIsChecked] = useState(false);
+
+  const decrementCount = () => {
+    setCount(count - 1);
+  };
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
   return (
     <Fragment>
-      <Steps />
-      <div className="max-w-[1120px] mx-auto">
-        <section className="sect_1">
-          <div className="my-5 md:flex pb-6 border-b border-[#D9D9D9]">
-            <div className="flex md:w-1/2 px-5">
-              <div className="w-1/2">
-                <p className="text-lg font-bold">ご予約内容</p>
-                <p>ご来店日</p>
-                <p>ご来店時間</p>
-                <p>人数</p>
+      <div className="max-w-[1120px] mx-auto md:mt-16 mt-8">
+        <div className="border-b border-[#D9D9D9] md:pb-10 pb-6">
+          <div className="md:px-0 px-5">
+            <h1 className="font-bold md:text-3xl text-xl">
+              しゃぶ葉 渋谷駅前店
+            </h1>
+            <div className="md:flex md:mt-12 mt-6">
+              <div className="flex md:w-1/2 md:bg-[#EDEDED] justify-between p-5 md:mr-3">
+                <div className="w-1/2">
+                  <p className="font-xl">人数</p>
+                </div>
+                <div className="text-center">
+                  <button
+                    className="btn bg-[#04512A] border-0 p-0 text-sm min-h-0 h-6 w-6 rounded-[4px] items-start"
+                    onClick={decrementCount}
+                  >
+                    <p className="text-white text-xl leading-none">-</p>
+                  </button>
+                  <input
+                    className="bg-transparent text-center text-lg w-[94px] appearance-none relative"
+                    type="number"
+                    name="number"
+                    id="number"
+                    value={`${count}`}
+                    onChange={(e) => setCount(parseInt(e.target.value))}
+                  />
+                  <button
+                    className="btn bg-[#04512A] border-0 p-0 text-sm min-h-0 h-6 w-6 rounded-[4px] items-start"
+                    onClick={incrementCount}
+                  >
+                    <p className="text-white text-xl leading-none">+</p>
+                  </button>
+                </div>
               </div>
-              <div className="w-1/2 text-right">
-                <p className="text-[#04512A] text-base underline underline-offset-4">
-                  変更する
-                </p>
-                <p>2023年03月17日(金)</p>
-                <p>11:00</p>
-                <p>3名</p>
-              </div>
-            </div>
-            <div className="flex md:w-1/2 px-5 mt-9 md:mt-0">
-              <div className="w-1/2">
-                <p className="text-lg font-bold">オプション選択</p>
-              </div>
-              <div className="w-1/2 text-right">
-                <p className="text-[#04512A] text-base underline underline-offset-4">
-                  変更する
-                </p>
+              <div className="flex md:w-1/2 md:bg-[#EDEDED] justify-between p-5 md:ml-3">
+                <p className="font-xl">時間</p>
+                <select
+                  className="bg-white border border-[#8E8E8E] rounded md:max-w-[412px] w-1/2 pl-6 text-base"
+                  name="time"
+                  id=""
+                  onChange={(e) => e.target.value}
+                  defaultValue="選択してください"
+                >
+                  <option disabled>選択してください</option>
+                  <option value="time1">Time 1</option>
+                  <option value="time2">Time 2</option>
+                  <option value="time3">Time 3</option>
+                </select>
               </div>
             </div>
           </div>
-        </section>
-        <section className="sect_2 px-5">
-          <ReservationForm />
-        </section>
+          <p className="md:text-lg text-sm md:mt-14 mt-9 md:px-0 px-5">
+            席のみのご予約となります。コースの選択については店舗にご来店後、各テーブルにてお伝えください。
+          </p>
+        </div>
+        <OptionSelection />
+        <Button text="入力・確認画面に進む" />
       </div>
     </Fragment>
   );
